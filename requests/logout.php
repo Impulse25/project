787d642dc98fe6116df696240c03a83e8c601ba0
Impulse25 <156745330+Impulse25@users.noreply.github.com>
2026-06-04 10:15:21 +0500

    Исправление логики работы logout

--- requests/logout.php
+++ requests/logout.php
@@ -2,9 +2,7 @@
 // logout.php — Выход из системы
 require_once 'config/db.php';
 require_once 'includes/auth.php';
-
 logout($pdo);
-
 // Редирект на главную с параметром для открытия формы входа
 header('Location: /?login=1');
 exit();
