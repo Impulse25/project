fb48c0e5e2a0dcbe7300e16b43d8bc46cd3b63ff
Almas <156745526+Almas289@users.noreply.github.com>
2026-06-16 15:51:12 +0500

    Delete achievements directory

--- achievements/actions/absence_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../includes/auth.php';
-require_once '../includes/functions.php';
-requireRole('admin','teacher');
-
-getPDO()->prepare("DELETE FROM absences WHERE id=?")->execute([(int)$_GET['id']]);
-header('Location: ' . SITE_URL . '/profile.php?id=' . (int)$_GET['user_id']);
--- achievements/actions/grade_save.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../includes/auth.php';
-require_once '../includes/functions.php';
-requireRole('admin','teacher');
-
-getPDO()->prepare("INSERT INTO grades (student_id,subject,grade,period,teacher_id) VALUES(?,?,?,?,?)")
-    ->execute([(int)$_POST['student_id'], $_POST['subject'], (int)$_POST['grade'], $_POST['period'] ?? '', $user['id']]);
-
-header('Location: ' . SITE_URL . '/profile.php?id=' . (int)$_POST['user_id']);
