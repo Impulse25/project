16f1d979fb3128fec8bfa8229968ca601975924c
zhenya <156745040+zhenya-golodnev@users.noreply.github.com>
2026-06-19 21:43:09 +0500

    Update index.php

--- umr/index.php
+++ umr/index.php
@@ -7,6 +7,7 @@ $isLoggedIn = isset($_SESSION['user_id']);
 $nameParts  = explode(' ', trim($userName));
 $initials   = implode('', array_map(fn($p) => mb_strtoupper(mb_substr($p,0,1)), array_slice($nameParts,0,2)));
 ?>
+
 <!DOCTYPE html>
 <html lang="ru" data-theme="light">
 <head>
